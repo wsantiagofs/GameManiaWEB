72f5ee911472c2658940b8a3b275ae914ce7453c
Wsanti <wsantiagoferreirasantos@gmail.com>
2022-07-28 17:47:39 -0300

    Dropdown/footer
    Adoção da funcionalidade de dropdown e footer modificado para ter mais responsividade.

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var btn_1 = document.querySelector(".drop_desktop_icon button")
+var btn_2 = document.querySelector(".drop_desktop button")
+var btn_3 = document.querySelector(".drop_mobile button")
+
+btn_1.addEventListener("click", function(){
+    this.classList.toggle("active");
+})
+
+btn_2.addEventListener("click", function(){
+    this.classList.toggle("active");
+})
+
+btn_3.addEventListener("click", function(){
+    this.classList.toggle("active");
+})
